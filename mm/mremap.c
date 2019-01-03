8368cf2642826b274be336df569db808db696aa7
Joel Fernandes (Google) <joel@joelfernandes.org>
2019-01-03 15:28:38 -0800

    mm: Speed up mremap by 20x on large regions.
    Android needs to mremap large regions of memory during memory management
    related operations.  The mremap system call can be really slow if THP is
    not enabled.  The bottleneck is move_page_tables, which is copying each
    pte at a time, and can be really slow across a large map.  Turning on
    THP may not be a viable option, and is not for us.  This patch speeds up
    the performance for non-THP system by copying at the PMD level when
    possible.
    
    The speedup is an order of magnitude on x86 (~20x).  On a 1GB mremap,
    the mremap completion times drops from 3.4-3.6 milliseconds to 144-160
    microseconds.
    
    Before:
    Total mremap time for 1GB data: 3521942 nanoseconds.
    Total mremap time for 1GB data: 3449229 nanoseconds.
    Total mremap time for 1GB data: 3488230 nanoseconds.
    
    After:
    Total mremap time for 1GB data: 150279 nanoseconds.
    Total mremap time for 1GB data: 144665 nanoseconds.
    Total mremap time for 1GB data: 158708 nanoseconds.
    
    If THP is enabled the optimization is mostly skipped except in certain
    situations.
    
    [joel@joelfernandes.org: fix 'move_normal_pmd' unused function warning]
      Link: http://lkml.kernel.org/r/20181108224457.GB209347@google.com
    Link: http://lkml.kernel.org/r/20181108181201.88826-3-joelaf@google.com
    
    Change-Id: Ia8bc5dcef6d8b496169f8e39e8ba8fee7344beb5

--- mm/mremap.c
+++ mm/mremap.c
@@ -304,7 +304,7 @@ unsigned long move_page_tables(struct vm_area_struct *vma,
 				continue;
 #endif
 		}
-		
+
 		if (pte_alloc(new_vma->vm_mm, new_pmd, new_addr))
 			break;
 		next = (new_addr + PMD_SIZE) & PMD_MASK;
