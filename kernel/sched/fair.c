a98bb2523c91d38307b76a81ffa40958b5ea66b0
Rik van Riel <riel@surriel.com>
2021-06-21 19:43:30 +0200

    sched/fair: Ensure that the CFS parent is added after unthrottling
    Ensure that a CFS parent will be in the list whenever one of its children is also
    in the list.
    
    A warning on rq->tmp_alone_branch != &rq->leaf_cfs_rq_list has been
    reported while running LTP test cfs_bandwidth01.
    
    Odin Ugedal found the root cause:
    
    	$ tree /sys/fs/cgroup/ltp/ -d --charset=ascii
    	/sys/fs/cgroup/ltp/
    	|-- drain
    	`-- test-6851
    	    `-- level2
    		|-- level3a
    		|   |-- worker1
    		|   `-- worker2
    		`-- level3b
    		    `-- worker3
    
    Timeline (ish):
    - worker3 gets throttled
    - level3b is decayed, since it has no more load
    - level2 get throttled
    - worker3 get unthrottled
    - level2 get unthrottled
      - worker3 is added to list
      - level3b is not added to list, since nr_running==0 and is decayed
    
     [ Vincent Guittot: Rebased and updated to fix for the reported warning. ]
    
    Fixes: a7b359fc6a37 ("sched/fair: Correctly insert cfs_rq's to list on unthrottle")
    Reported-by: Sachin Sant <sachinp@linux.vnet.ibm.com>
    Suggested-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Rik van Riel <riel@surriel.com>
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Tested-by: Sachin Sant <sachinp@linux.vnet.ibm.com>
    Acked-by: Odin Ugedal <odin@uged.al>
    Link: https://lore.kernel.org/r/20210621174330.11258-1-vincent.guittot@linaro.org

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3725,6 +3725,9 @@ static inline bool cfs_rq_is_decayed(struct cfs_rq *cfs_rq)
 	if (child_cfs_rq_on_list(cfs_rq))
 		return false;
 
+	if (child_cfs_rq_on_list(cfs_rq))
+		return false;
+
 	return true;
 }
 
