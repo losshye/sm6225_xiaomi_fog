f8eafd6c9da0d3ccf9556c36fe0c2b49e22017e9
Will Deacon <will@kernel.org>
2021-07-30 12:24:33 +0100

    sched: Introduce task_struct::user_cpus_ptr to track requested affinity
    In preparation for saving and restoring the user-requested CPU affinity
    mask of a task, add a new cpumask_t pointer to 'struct task_struct'.
    
    If the pointer is non-NULL, then the mask is copied across fork() and
    freed on task exit.
    
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <Valentin.Schneider@arm.com>
    Link: https://lore.kernel.org/r/20210730112443.23245-7-will@kernel.org

--- kernel/fork.c
+++ kernel/fork.c
@@ -406,6 +406,7 @@ void put_task_stack(struct task_struct *tsk)
 void free_task(struct task_struct *tsk)
 {
 	cpufreq_task_times_exit(tsk);
+	release_user_cpus_ptr(tsk);
 	scs_release(tsk);
 
 #ifndef CONFIG_THREAD_INFO_IN_TASK
@@ -908,6 +909,7 @@ static struct task_struct *dup_task_struct(struct task_struct *orig, int node)
 #ifdef CONFIG_STACKPROTECTOR
 	tsk->stack_canary = get_random_canary();
 #endif
+	dup_user_cpus_ptr(tsk, orig, node);
 
 	/* One for the user space visible state that goes away when reaped. */
 	refcount_set(&tsk->rcu_users, 1);
