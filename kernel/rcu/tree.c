ca898eb0e1dec681ebbe38d46a4b13ed5ac89e94
Joel Fernandes (Google) <joel@joelfernandes.org>
2022-06-02 10:06:43 +0200

    UPSTREAM: rcu/kvfree: Remove useless monitor_todo flag
    monitor_todo is not needed as the work struct already tracks
    if work is pending. Just use that to know if work is pending
    using schedule_delayed_work() helper.
    
    BUG=b:222463781
    TEST=powerIdle lab tests.
    
    Change-Id: If9abe05558a6eaeb75989e0c96f56e76668dcc10
    Signed-off-by: Joel Fernandes (Google) <joel@joelfernandes.org>
    Signed-off-by: Uladzislau Rezki (Sony) <urezki@gmail.com>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>
    Reviewed-by: Neeraj Upadhyay <quic_neeraju@quicinc.com>
    (cherry picked from commit 82d26c36cc68e781400eb4e541f943008208f2d6)
    Signed-off-by: Joel Fernandes <joelaf@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4004847
    Reviewed-by: Ross Zwisler <zwisler@google.com>
    Reviewed-by: Sean Paul <sean@poorly.run>
    (cherry picked from commit 0f1d9b7fb6c1ee0c50e1bad6d3fbd9f6cc98e476)
    Signed-off-by: Panchajanya1999 <kernel@panchajanya.dev>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -3133,7 +3133,6 @@ struct kfree_rcu_cpu_work {
  * @krw_arr: Array of batches of kfree_rcu() objects waiting for a grace period
  * @lock: Synchronize access to this structure
  * @monitor_work: Promote @head to @head_free after KFREE_DRAIN_JIFFIES
- * @monitor_todo: Tracks whether a @monitor_work delayed work is pending
  * @initialized: The @rcu_work fields have been initialized
  * @count: Number of objects for which GP not started
  * @bkvcache:
@@ -3158,7 +3157,6 @@ struct kfree_rcu_cpu {
 	struct kfree_rcu_cpu_work krw_arr[KFREE_N_BATCHES];
 	raw_spinlock_t lock;
 	struct delayed_work monitor_work;
-	bool monitor_todo;
 	bool initialized;
 	int count;
 
@@ -3425,13 +3423,8 @@ static void kfree_rcu_monitor(struct work_struct *work)
 	}
 }
 
-        if (!krcp->bkvhead[0] && !krcp->bkvhead[1] && !krcp->head)
-		krcp->monitor_todo = false;
-		
-		else
-
+        if (need_offload_krc(krcp))
 	schedule_delayed_monitor_work(krcp);
-
 	raw_spin_unlock_irqrestore(&krcp->lock, flags);
 
 
@@ -3603,11 +3596,9 @@ void kvfree_call_rcu(struct rcu_head *head, rcu_callback_t func)
 	kmemleak_ignore(ptr);
 
 	// Set timer to drain after KFREE_DRAIN_JIFFIES.
-	if (rcu_scheduler_active == RCU_SCHEDULER_RUNNING &&
-	    !krcp->monitor_todo) {
-		krcp->monitor_todo = true;
+	if (rcu_scheduler_active == RCU_SCHEDULER_RUNNING);
 		schedule_delayed_monitor_work(krcp);
-	}
+	
 
 unlock_return:
 	krc_this_cpu_unlock(krcp, flags);
@@ -3682,12 +3673,7 @@ void __init kfree_rcu_scheduler_running(void)
 		struct kfree_rcu_cpu *krcp = per_cpu_ptr(&krc, cpu);
 
 		raw_spin_lock_irqsave(&krcp->lock, flags);
-		if ((!krcp->bkvhead[0] && !krcp->bkvhead[1] && !krcp->head) ||
-				krcp->monitor_todo) {
-			raw_spin_unlock_irqrestore(&krcp->lock, flags);
-			continue;
-		}
-		krcp->monitor_todo = true;
+		if (need_offload_krc(krcp))
 	    schedule_delayed_monitor_work(krcp);
 		raw_spin_unlock_irqrestore(&krcp->lock, flags);
 	}
