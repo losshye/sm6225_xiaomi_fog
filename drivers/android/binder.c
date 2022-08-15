1736305ea4c921ae2eaf213b8ba887b04aff2718
ChungKai Mei <chungkai@google.com>
2022-08-15 11:42:13 +0000

    ANDROID: binder: add support for uclamp inheritance
    We may still have long binder transaction due to
    insufficient uclamp, so expand binder_prioity to
    inherit uclamp as well.
    
    Bug: 226003124
    Signed-off-by: ChungKai Mei <chungkai@google.com>
    Change-Id: I307ab812638eeea1ca3e80ae07dee05fb9797dd6

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -685,32 +685,18 @@ static bool binder_supported_policy(int policy)
 #ifdef CONFIG_UCLAMP_TASK
 static void set_binder_prio_uclamp(struct binder_priority *prio, struct task_struct *task)
 {
-	if (!uclamp_is_used())
-		return;
-
-	if (task) {
-		prio->uclamp[UCLAMP_MIN] = task->uclamp_req[UCLAMP_MIN].value;
-		prio->uclamp[UCLAMP_MAX] = task->uclamp_req[UCLAMP_MAX].value;
-	} else {
-		prio->uclamp[UCLAMP_MIN] = 0;
-		prio->uclamp[UCLAMP_MAX] = SCHED_CAPACITY_SCALE;
-	}
+	prio->uclamp[UCLAMP_MIN] = task->uclamp_req[UCLAMP_MIN].value;
+	prio->uclamp[UCLAMP_MAX] = task->uclamp_req[UCLAMP_MAX].value;
 }
 
 static void set_inherited_uclamp(struct binder_transaction *t)
 {
-	if (!uclamp_is_used())
-		return;
-
 	t->priority.uclamp[UCLAMP_MIN] = uclamp_eff_value(current, UCLAMP_MIN);
 	t->priority.uclamp[UCLAMP_MAX] = uclamp_eff_value(current, UCLAMP_MAX);
 }
 
 static bool is_uclamp_equal(struct task_struct *task, const struct binder_priority *desired)
 {
-	if (!uclamp_is_used())
-		return true;
-
 	return task->uclamp_req[UCLAMP_MIN].value == desired->uclamp[UCLAMP_MIN]
 		&& task->uclamp_req[UCLAMP_MAX].value == desired->uclamp[UCLAMP_MAX];
 }
@@ -749,14 +735,10 @@ static void binder_do_set_priority(struct binder_thread *thread,
 	bool has_cap_nice;
 	unsigned int policy = desired->sched_policy;
 	struct sched_attr attrs = {
-		.sched_flags = SCHED_FLAG_RESET_ON_FORK
+		.sched_flags = SCHED_FLAG_UTIL_CLAMP | SCHED_FLAG_RESET_ON_FORK,
+		.sched_util_min = desired->uclamp[UCLAMP_MIN],
+		.sched_util_max = desired->uclamp[UCLAMP_MAX],
 	};
-
-	if (uclamp_is_used()) {
-		attrs.sched_flags |= SCHED_FLAG_UTIL_CLAMP;
-		attrs.sched_util_min = desired->uclamp[UCLAMP_MIN];
-		attrs.sched_util_max = desired->uclamp[UCLAMP_MAX];
-	}
 
 	if (task->policy == policy && task->normal_prio == desired->prio
 		&& is_uclamp_equal(task, desired)) {
@@ -6170,7 +6152,7 @@ static int binder_open(struct inode *nodp, struct file *filp)
 		proc->default_priority.prio = NICE_TO_PRIO(0);
 	}
 
-	set_binder_prio_uclamp(&proc->default_priority, NULL);
+	set_binder_prio_uclamp(&proc->default_priority, current);
 
 	/* binderfs stashes devices in i_private */
 	if (is_binderfs_device(nodp)) {
