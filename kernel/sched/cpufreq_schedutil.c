ccf7413e6b6ce2b204ccc002de75305e070b3b75
Sultan Alsawaf <sultan@kerneltoast.com>
2023-09-05 00:37:56 +0000

    kernel: schedutil: Allow CPU frequency changes to be amended before they're set If the last CPU frequency selected isn't set before a new CPU frequency selection arrives, then use the new selection immediately to avoid using a stale frequency choice. This improves both performance and energy by more closely tracking the scheduler's latest decisions.
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -136,10 +136,17 @@ static bool sugov_up_down_rate_limit(struct sugov_policy *sg_policy, u64 time,
 static bool sugov_update_next_freq(struct sugov_policy *sg_policy, u64 time,
 				   unsigned int next_freq)
 {
+
+	/*
+	 * If a work is in progress then it means the last frequency wasn't set
+	 * yet, so we can still change our mind and set a different frequency.
+	 */
+	bool ignore_rate_limit = sg_policy->work_in_progress;
+
 	if (sg_policy->next_freq == next_freq)
 		return false;
 
-	if (sugov_up_down_rate_limit(sg_policy, time, next_freq)) {
+	if (!ignore_rate_limit && sugov_up_down_rate_limit(sg_policy, time, next_freq)) {
 		/* Restore cached freq as next_freq is not changed */
 		sg_policy->cached_raw_freq = sg_policy->prev_cached_raw_freq;
 		return false;
