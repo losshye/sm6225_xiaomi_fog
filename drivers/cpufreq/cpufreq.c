c407f526bd441e4d86160244b30e0cad9e62b2b8
Dwyor Tmx <akunfreefire15417@gmail.com>
2024-08-25 05:08:14 +0000

    Revert "cpufreq: Always respect min freq"
    This reverts commit 4f8d3feaf78c34a1b4ba4e671ee3fd550be07de1.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -732,12 +732,12 @@ static ssize_t store_##file_name					\
 	if (ret != 1)							\
 		return -EINVAL;						\
 									\
-	if (&policy->object == &policy->max)				\
-		if (val < policy->min)					\
-			val = policy->min;				\
+	temp = new_policy.object;					\
+	ret = cpufreq_set_policy(policy, &new_policy);		\
+	if (!ret)							\
+		policy->user_policy.object = temp;			\
 									\
-	ret = freq_qos_update_request(policy->object##_freq_req, val);\
-	return ret >= 0 ? count : ret;					\
+	return ret ? ret : count;					\
 }
 
 store_one(scaling_min_freq, min);
