e98138cf0920f301a3b505d76bf92f6672d06c7e
Dwyor Tmx <akunfreefire15417@gmail.com>
2024-08-25 05:08:40 +0000

    Revert "cpuidle: Make notifier no-op when restricted to WFI"
    This reverts commit 9823f3e2331e583d1cf1d0fb1a833d45e247ae3d.

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -690,8 +690,7 @@ static void wake_up_idle_cpus(void *v)
 static int cpuidle_latency_notify(struct notifier_block *b,
 		unsigned long l, void *v)
 {
-	if (!lpm_sleep_disabled())
-		wake_up_idle_cpus(v);
+	wake_up_idle_cpus(v);
 	return NOTIFY_OK;
 }
 
