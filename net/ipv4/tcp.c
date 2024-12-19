0945c46f7bd8128d23cb6d61852696f95a9c7ea1
Dwyor Tmx <akunfreefire15417@gmail.com>
2024-12-19 13:39:11 +0000

    Revert "net-tcp: add fast_ack_mode=1: skip rwin check in tcp_fast_ack_mode__tcp_ack_snd_check()"
    This reverts commit 1075a22880d35e967f3eccfdf22a573ad604e49c.

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -2686,7 +2686,8 @@ int tcp_disconnect(struct sock *sk, int flags)
 	tp->bytes_retrans = 0;
 	tp->data_segs_in = 0;
 	tp->data_segs_out = 0;
-	tp->fast_ack_mode = 0;
+	tp->dsack_dups = 0;
+	tp->reord_seen = 0;
 
 	/* Clean up fastopen related fields */
 	tcp_free_fastopen_req(tp);
