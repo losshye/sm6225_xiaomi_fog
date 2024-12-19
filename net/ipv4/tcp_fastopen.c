2d2eeaf3c140ccffcfcdf6521113ef4661dcb83d
Dwyor Tmx <akunfreefire15417@gmail.com>
2024-12-19 13:49:17 +0000

    Revert "tcp: fastopen: Enable support for TCP fast open when serving"
    This reverts commit 08ad0b85ed900fcf13861b9d8baeacd6a7591985.

--- net/ipv4/tcp_fastopen.c
+++ net/ipv4/tcp_fastopen.c
@@ -10,19 +10,18 @@
 #include <net/inetpeer.h>
 #include <net/tcp.h>
 
-int sysctl_tcp_fastopen __read_mostly = TFO_CLIENT_ENABLE |
-					TFO_SERVER_ENABLE |
-					TFO_CLIENT_NO_COOKIE |
-					TFO_SERVER_COOKIE_NOT_REQD |
-					TFO_SERVER_WO_SOCKOPT1;
-
-struct tcp_fastopen_context __rcu *tcp_fastopen_ctx;
-
-static DEFINE_SPINLOCK(tcp_fastopen_ctx_lock);
-
-void tcp_fastopen_init_key_once(bool publish)
+void tcp_fastopen_init_key_once(struct net *net)
 {
-	static u8 key[TCP_FASTOPEN_KEY_LENGTH];
+	u8 key[TCP_FASTOPEN_KEY_LENGTH];
+	struct tcp_fastopen_context *ctxt;
+
+	rcu_read_lock();
+	ctxt = rcu_dereference(net->ipv4.tcp_fastopen_ctx);
+	if (ctxt) {
+		rcu_read_unlock();
+		return;
+	}
+	rcu_read_unlock();
 
 	/* tcp_fastopen_reset_cipher publishes the new context
 	 * atomically, so we allow this race happening here.
