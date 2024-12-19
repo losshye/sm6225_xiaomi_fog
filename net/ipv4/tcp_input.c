cc673a72bb74498eed68073e63b06c9a1610b311
Dwyor Tmx <akunfreefire15417@gmail.com>
2024-12-19 14:01:11 +0000

    Revert "BACKPORT: tcp: mandate a one-time immediate ACK"
    This reverts commit 4f84090517754cd4616de527ab6a8c84e5e1220f.

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -5284,11 +5284,9 @@ static void __tcp_ack_snd_check(struct sock *sk, int ofo_possible)
 	     __tcp_select_window(sk) >= tp->rcv_wnd)) ||
 	    /* We ACK each frame or... */
 	    tcp_in_quickack_mode(sk) ||
-	    /* We have out of order data or... */
-	    (ofo_possible && !RB_EMPTY_ROOT(&tp->out_of_order_queue)) ||
 	    /* Protocol state mandates a one-time immediate ACK */
 	    inet_csk(sk)->icsk_ack.pending & ICSK_ACK_NOW) {
-		/* Then ack it now */
+send_now:
 		tcp_send_ack(sk);
 		return;
 	}
