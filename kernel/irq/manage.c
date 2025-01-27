45a92809ea94f1297e6afcd8faef7636510dea37
Dwyor Tmx <akunfreefire15417@gmail.com>
2025-01-27 02:48:49 +0000

    Revert "kernel: irq: Switch to bi-cluster API for marking IRQ threads as critical"
    This reverts commit b63e8ba68b394c0a7acad47941cc7b7145f1cdb7.

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -27,6 +27,7 @@
 static LIST_HEAD(perf_crit_irqs);
 static DEFINE_RAW_SPINLOCK(perf_irqs_lock);
 static int perf_cpu_index = -1;
+static int prime_cpu_index = -1;
 static int lp_cpu_index = -1;
 static bool perf_crit_suspended;
 
@@ -1281,6 +1282,8 @@ static void affine_one_perf_thread(struct irqaction *action)
 
 	if (action->flags & IRQF_PERF_AFFINE)
 		mask = cpu_perf_mask;
+	else if (action->flags & IRQF_PRIME_AFFINE)
+		mask = cpu_prime_mask;
 	else
 		mask = cpu_lp_mask;
 
@@ -1306,6 +1309,9 @@ static void affine_one_perf_irq(struct irq_desc *desc, unsigned int perf_flag)
 	if (perf_flag & IRQF_PERF_AFFINE) {
 		mask = cpu_perf_mask;
 		mask_index = &perf_cpu_index;
+	} else if (perf_flag & IRQF_PRIME_AFFINE) {
+		mask = cpu_prime_mask;
+		mask_index = &prime_cpu_index;
 	} else {
 		mask = cpu_lp_mask;
 		mask_index = &lp_cpu_index;
@@ -1382,6 +1388,7 @@ void reaffine_perf_irqs(bool from_hotplug)
 	if (!from_hotplug || !perf_crit_suspended) {
 		perf_crit_suspended = false;
 		perf_cpu_index = -1;
+		prime_cpu_index = -1;
 		lp_cpu_index = -1;
 		list_for_each_entry(desc, &perf_crit_irqs, perf_list) {
 			raw_spin_lock(&desc->lock);
@@ -1665,7 +1672,7 @@ __setup_irq(unsigned int irq, struct irq_desc *desc, struct irqaction *new)
 			irqd_set(&desc->irq_data, IRQD_NO_BALANCING);
 		}
 
-		if (new->flags & (IRQF_PERF_AFFINE | IRQF_LITTLE_AFFINE)) {
+		if (new->flags & (IRQF_PERF_AFFINE | IRQF_PRIME_AFFINE | IRQF_LITTLE_AFFINE)) {
 			affine_one_perf_thread(new);
 			irqd_set(&desc->irq_data, IRQD_PERF_CRITICAL);
 			*old_ptr = new;
