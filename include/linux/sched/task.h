23e946ed4aa479f9d5be7b4377dc70d26995c1e8
Dwyor Tmx <akunfreefire15417@gmail.com>
2025-01-28 01:13:28 +0000

    sched: checkout to redbull

--- include/linux/sched/task.h
+++ include/linux/sched/task.h
@@ -37,7 +37,6 @@ extern void sched_post_fork(struct task_struct *p);
 extern void sched_dead(struct task_struct *p);
 
 void __noreturn do_task_dead(void);
-void __noreturn make_task_dead(int signr);
 
 extern void proc_caches_init(void);
 
@@ -101,7 +100,6 @@ static inline void put_task_struct(struct task_struct *t)
 }
 
 struct task_struct *task_rcu_dereference(struct task_struct **ptask);
-void put_task_struct_rcu_user(struct task_struct *task);
 
 #ifdef CONFIG_ARCH_WANTS_DYNAMIC_TASK_STRUCT
 extern int arch_task_struct_size __read_mostly;
