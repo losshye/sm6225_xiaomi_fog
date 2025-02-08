cff868120e045ad8975910c83b5d6cef6957f45e
An. <noegrahachan@gmail.com>
2025-02-08 12:16:30 +0000

    drivers: nt36525b_spi: more changes
    - drop lct_nvt_tp_gesture_callback, touch gestures now done with ts->is_gesture_mode = true|false
    - drop set_lcd_gpio_keep_high
    - drop nvt_ts_get|enable_regulator, it was not used and safe to remove it

--- drivers/input/touchscreen/nt36525b_spi/nt36xxx.c
+++ drivers/input/touchscreen/nt36525b_spi/nt36xxx.c
@@ -177,7 +177,8 @@ static ssize_t double_tap_store(struct kobject *kobj,
     rc = kstrtoint(buf, 10, &val);
     if (rc)
     return -EINVAL;
-    lct_nvt_tp_gesture_callback(!!val);
+
+    ts->is_gesture_mode = !!val;
     return count;
 }
 static struct tp_common_ops double_tap_ops = {
