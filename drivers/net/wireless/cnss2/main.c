19f42d0b8a7ba72f8e028018de89be6d307c2da5
kvsnr113 <kvsnrprojkt113@gmail.com>
2025-03-02 00:59:42 +0700

    net/cnss2: don't compile debug
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- drivers/net/wireless/cnss2/main.c
+++ drivers/net/wireless/cnss2/main.c
@@ -2753,8 +2753,6 @@ static int cnss_probe(struct platform_device *plat_dev)
 	if (ret)
 		goto deinit_event_work;
 
-	cnss_debugfs_create(plat_priv);
-
 	ret = cnss_misc_init(plat_priv);
 	if (ret)
 		goto destroy_debugfs;
@@ -2771,7 +2769,6 @@ static int cnss_probe(struct platform_device *plat_dev)
 	return 0;
 
 destroy_debugfs:
-	cnss_debugfs_destroy(plat_priv);
 	cnss_qmi_deinit(plat_priv);
 deinit_event_work:
 	cnss_event_work_deinit(plat_priv);
@@ -2804,7 +2801,6 @@ static int cnss_remove(struct platform_device *plat_dev)
 	cnss_unregister_ims_service(plat_priv);
 	cnss_unregister_coex_service(plat_priv);
 	cnss_misc_deinit(plat_priv);
-	cnss_debugfs_destroy(plat_priv);
 	cnss_qmi_deinit(plat_priv);
 	cnss_event_work_deinit(plat_priv);
 	cnss_remove_sysfs(plat_priv);
@@ -2834,10 +2830,7 @@ static int __init cnss_initialize(void)
 {
 	int ret = 0;
 
-	cnss_debug_init();
 	ret = platform_driver_register(&cnss_platform_driver);
-	if (ret)
-		cnss_debug_deinit();
 
 	return ret;
 }
@@ -2845,7 +2838,6 @@ static int __init cnss_initialize(void)
 static void __exit cnss_exit(void)
 {
 	platform_driver_unregister(&cnss_platform_driver);
-	cnss_debug_deinit();
 }
 
 module_init(cnss_initialize);
