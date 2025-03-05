f7320418c159b6c30f5abebe21847eaf863d81e9
Dwyor Tmx <akunfreefire15417@gmail.com>
2025-03-05 02:32:25 +0000

    fixup

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -19,6 +19,8 @@
 #include <linux/shmem_fs.h>
 #include <linux/uaccess.h>
 #include <linux/pkeys.h>
+#include <linux/mm_inline.h>
+#include <linux/ctype.h>
 #ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
 #include <linux/susfs_def.h>
 #endif
